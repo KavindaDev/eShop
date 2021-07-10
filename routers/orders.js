b944eac798e1c3faebea8d0b81469111a3285784
Kavinda <kavinda@surreytechconsulting.lk>
2021-07-10 18:37:24 +0530

    added delet order and update order

--- routers/orders.js
+++ routers/orders.js
@@ -69,4 +69,41 @@ router.post(`/`, async (req, res) => {
     res.send(order);
 })
 
+
+//delete by id *then* method
+router.delete('/:id', (req, res)=>{
+    Order.findByIdAndRemove(req.params.id).then(async order =>{
+        if(order) {
+            await order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            })
+            return res.status(200).json({success: true, message: 'the order is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "order not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+
+
+//update order by id
+router.put(`/:id`, async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+           status: req.body.status 
+        },
+        {new: true}
+    )
+
+    if(!order)
+    res.status(400).send('the {{id}} order cannot be updated')
+
+    res.send(order);
+})
+
+
+
 module.exports =router;
